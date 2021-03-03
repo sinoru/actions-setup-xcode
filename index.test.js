7c9b5204a4442392c7438acc9089f80f3adb9e58
Jaehong Kang <sinoru@me.com>
2021-03-04 00:33:19 +0900

    Update test

--- index.test.js
+++ index.test.js
@@ -1,22 +1,13 @@
 const process = require('process');
-const util = require('util');
-const execFile = util.promisify(require('child_process').execFile);
+const cp = require('child_process');
 const path = require('path');
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', async () => {
-  try {
-    const ip = path.join(__dirname, 'index.mjs');
-    const env = {
-      ...process.env,
-      'INPUT_XCODE-VERSION': '12.4'
-    };
-
-    const { stdout, stderr } = await execFile('node', [ip], {env});
-    console.log(stdout);
-    console.error(stderr);
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-}, 300000);
+test('test runs', () => {
+  const env = {
+    ...process.env,
+    'INPUT_XCODE-VERSION': '12.4'
+  };
+  const ip = path.join(__dirname, 'index.js');
+  console.log(cp.execFileSync('node', [ip],, {env}).toString());
+});
